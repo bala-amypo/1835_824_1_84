2bd338e5331f4bf7dd699240eba2bbcead962fc5
bala-amypo <bala@amypo.in>
2025-12-17 03:58:30 +0000

    User commit

--- src/main/java/com/example/demo/model/ServicePart.java
+++ src/main/java/com/example/demo/model/ServicePart.java
@@ -2,5 +2,15 @@ package com.example.demo.model;
 public class ServicePart{
     private Long id;
     private String serviceType;
-    private String partName
+    private String partName;
+    private String partNumber;
+    private BigDecimal cost;
+    private Integer quantity;
+    
+    public Long getId(){
+        return id;
+    }
+    public void setId(String vehicle){
+        this.vehicle=vehicle;
+    }
 }
