d0ea3cf85fc695b912d848bc70c80a984a4fbf68
bala-amypo <bala@amypo.in>
2025-12-17 10:16:24 +0000

    User commit

--- src/main/java/com/example/demo/model/Garage.java
+++ src/main/java/com/example/demo/model/Garage.java
@@ -1,9 +1,12 @@
 package com.example.demo.model;
-import jakarta.persistence.Entity;
+import jakarta.persistence.entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 @Entity
 public class Garage{
     @Id
+    GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
     private String garagename;
     private String address;
