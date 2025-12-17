80f1ed3a984b98eb82ebb0747dbce6971cbe5c43
bala-amypo <bala@amypo.in>
2025-12-17 06:27:59 +0000

    User commit

--- src/main/java/com/example/demo/model/VerificationLog.java
+++ src/main/java/com/example/demo/model/VerificationLog.java
@@ -3,5 +3,27 @@ public class VerificationLog{
     private Long id;
     private String ServiceEntry;
     private Timestamp verifiedAt=new;
-    private 
+    private Boolean verifiedbySystem;
+    private String notes;
+
+    public Long getId(){
+        return id;
+    }
+    public void setId(long id){
+        this.id=id;
+    }
+
+     public String getServiceEntry(){
+        return ServiceEntry;
+    }
+    public void setServiceEntry(String ServiceEntry){
+        this.serviceEntry=serviceEntry;
+    }
+
+     public Timestamp getVerifiedAt(){
+        return recordedAt;
+    }
+    public void setRecordedAt(Timestamp recordedAt){
+        this.recordedAt=recordedAt;
+    }
 }
