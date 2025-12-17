6151049c1e426e4f29968b2376d0cd38cca0102b
bala-amypo <bala@amypo.in>
2025-12-17 06:07:40 +0000

    User commit

--- src/main/java/com/example/demo/model/Vehicle.java
+++ src/main/java/com/example/demo/model/Vehicle.java
@@ -8,4 +8,39 @@ public class Vehicle{
     private Long ownerId;
     private Boolean active;
     private Timestamp createdAt;
+
+    public Long getId(){
+        return id;
+    }
+    public void setId(long id){
+        this.id=id;
+    }
+
+    public String getVin(){
+        return vin;
+    }
+    public void setVin(String vin){
+        this.vin=vin;
+    }
+
+     public String getMake(){
+        return make;
+    }
+    public void setMake(String make){
+        this.make=make;
+    }
+
+     public String getModel(){
+        return model;
+    }
+    public void setModel(String model){
+        this.model=model;
+    }
+
+     public St getYear(){
+        return vin;
+    }
+    public void setVin(String vin){
+        this.vin=vin;
+    }
 }
