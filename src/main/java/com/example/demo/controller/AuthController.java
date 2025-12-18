87405c89a39b26ecded61e418a0bf118c2a8f382
bala-amypo <bala@amypo.in>
2025-12-18 17:37:54 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,4 +1,15 @@
 package com.example.demo.controller;
 import org.springframework.web.bind.annonation.RestController;
 import org.springframework.beans.factory.annonation.Autowired;
-import com.example.demo.;
+import com.example.demo.entity.Garage;
+import com.example.demo.service.GarageService;
+import org.springframework.web.bind.annonation.PostMapping;
+import org.springframework.web.bind.annonation.RequestBody;
+
+@RestController
+public class Garage{
+    @Autowired
+    GarageService gs;
+    @PostMapping("/UserProfile")
+    public 
+}
