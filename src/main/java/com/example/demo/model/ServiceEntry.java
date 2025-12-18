f24c69ec4095aad1be62cb5b86ba83eead3a2c0a
bala-amypo <bala@amypo.in>
2025-12-18 10:35:54 +0000

    User commit

--- src/main/java/com/example/demo/model/ServiceEntry.java
+++ src/main/java/com/example/demo/model/ServiceEntry.java
@@ -8,7 +8,7 @@ import java.util.Date;
 @Entity
 public class ServiceEntry{
     @Id
-     @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
     private String vehicle;
     private String garage;
--- src/main/java/com/example/demo/repository/VerificationLogRepository.java
+++ src/main/java/com/example/demo/repository/VerificationLogRepository.java
@@ -1,6 +1,8 @@
 package com.example.demo.repository;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.model.VerificationLogrepo;
+import com.example.demo.model.VerificationLog;
+
 public interface VerificationLogRepository extends JpaRepository<VerificationLog,Long>{
 
 }
