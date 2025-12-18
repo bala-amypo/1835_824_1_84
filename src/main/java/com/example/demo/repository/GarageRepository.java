c0f8e599fc7f0cf666c6ae693c2048b229642805
bala-amypo <bala@amypo.in>
2025-12-18 10:05:54 +0000

    User commit

--- src/main/java/com/example/demo/repository/GarageRepository.java
+++ src/main/java/com/example/demo/repository/GarageRepository.java
@@ -1,6 +1,6 @@
-package com.example.demo.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.model.Garage;
-public interface GarageRepository extends JpaRepository<Garage,Long>{
+// package com.example.demo.repository;
+// import org.springframework.data.jpa.repository.JpaRepository;
+// import com.example.demo.model.Garage;
+// public interface GarageRepository extends JpaRepository<Garage,Long>{
     
-}
+// }
--- src/main/java/com/example/demo/repository/ServiceEntryRepository.java
+++ src/main/java/com/example/demo/repository/ServiceEntryRepository.java
@@ -1,6 +1,6 @@
-// package com.example.demo.repository;
-// import org.springframework.data.jpa.repository.JpaRepository;
-// import com.example.demo.model.ServiceEntry;
-// public interface ServiceEntryRepository extends JpaRepository<ServiceEntry,Long>{
-
-// }
+package com.example.demo.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.model.ServiceEntry;
+public interface  ServiceEntryRepository extends JpaRepository<ServiceEntry,Long>{
+    
+}
