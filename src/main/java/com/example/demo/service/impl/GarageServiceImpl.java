b3229c437ca2c53eae0c374d8c0544aa950a146f
bala-amypo <bala@amypo.in>
2025-12-18 17:03:52 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/GarageServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/GarageServiceImpl.java
@@ -1,3 +1,10 @@
 package com.example.demo.service.impl;
 import com.example.demo.repository.GarageRepository;
-import com.example.demo.entity.
+import com.example.demo.entity.Garage;
+import com.example.demo.service.GarageService;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+@Service
+public class 
