fbc18dc8bf62861dcfc0f1b2219e2fafba00c945
bala-amypo <bala@amypo.in>
2025-12-18 09:55:54 +0000

    User commit

--- src/main/java/com/example/demo/repository/ServicePartRepository.java
+++ src/main/java/com/example/demo/repository/ServicePartRepository.java
@@ -1,6 +1,6 @@
-package com.example.demo.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.model.ServicePart;
-public interface ServicePartRepository extends JpaRepository<ServicePart,Long>{
+// package com.example.demo.repository;
+// import org.springframework.data.jpa.repository.JpaRepository;
+// import com.example.demo.model.ServicePart;
+// public interface ServicePartRepository extends JpaRepository<ServicePart,Long>{
 
-}
+// }
--- src/main/java/com/example/demo/repository/VerificationLogRepository.java
+++ src/main/java/com/example/demo/repository/VerificationLogRepository.java
@@ -1,6 +1,6 @@
-package com.example.demo.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.model.VerificationLogrepo;
-public interface VerificationLogRepository extends JpaRepository<VerificationLog,Long>{
+// package com.example.demo.repository;
+// import org.springframework.data.jpa.repository.JpaRepository;
+// import com.example.demo.model.VerificationLogrepo;
+// public interface VerificationLogRepository extends JpaRepository<VerificationLog,Long>{
 
-}
+// }
