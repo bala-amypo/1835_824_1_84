54d967cfeb36c11df92613939cefe8d4d05a0ced
bala-amypo <bala@amypo.in>
2025-12-18 17:16:54 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -1,19 +1,19 @@
 package com.example.demo.service.impl;
-import com.example.demo.repository.UserServiceRepository;
-import com.example.demo.entity.Garage;
-import com.example.demo.service.GarageService;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.entity.User;
+import com.example.demo.service.UserService;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 
 
 @Service
-public class GarageServiceImpl implements GarageService{
+public class UserServiceImpl implements UserService{
 
 @Autowired
-GarageRepository gr;
-public Garage createGarage(Garage gar){
+UserRepository ur;
+public User createUser(User spa){
 
-    return gr.save(gar);
+    return spr.save(spa);
 }
 
 
