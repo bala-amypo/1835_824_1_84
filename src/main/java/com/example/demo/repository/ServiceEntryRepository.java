90aa1bff2e0cdcb18fe3092cb4cae69532a78bba
bala-amypo <bala@amypo.in>
2025-12-18 10:15:54 +0000

    User commit

--- src/main/java/com/example/demo/repository/ServiceEntryRepository.java
+++ src/main/java/com/example/demo/repository/ServiceEntryRepository.java
@@ -1,6 +1,8 @@
 package com.example.demo.repository;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.model.ServiceEntry;
+
 public interface ServiceEntryRepository extends JpaRepository<ServiceEntry,Long>{
     
 }
