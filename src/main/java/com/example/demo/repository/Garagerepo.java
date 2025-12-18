8be27c3c3f08d7290cd87d5991ac8f9a383edf95
bala-amypo <bala@amypo.in>
2025-12-18 06:50:51 +0000

    User commit

--- src/main/java/com/example/demo/repository/Garagerepo.java
+++ src/main/java/com/example/demo/repository/Garagerepo.java
@@ -1,4 +1,6 @@
 package com.example.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.Entityclass.User;
-public interface Garagerepo extends JpaRepository
+public interface Garagerepo extends JpaRepository<Garage,Long>{
+    
+}
