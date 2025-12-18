4fe60d4cecb3959c368bc03e21bc5311297bb076
bala-amypo <bala@amypo.in>
2025-12-18 17:10:54 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/ServiceEntryServiceImpl.java
@@ -0,0 +1,20 @@
+package com.example.demo.service.impl;
+import com.example.demo.repository.ServiceEntryRepository;
+import com.example.demo.entity.ServiceEntry;
+import com.example.demo.service.ServiceEntryService;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+@Service
+public class ServiceEntryServiceImpl implements ServiceEntryService{
+
+@Autowired
+ServiceEntryRepository ser;
+public ServiceEntry createServiceEntry(ServiceEntry g){
+
+    return gr.save(gar);
+}
+
+
+}
