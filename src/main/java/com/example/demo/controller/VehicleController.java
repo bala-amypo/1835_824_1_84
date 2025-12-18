136f8d7fb139985ab71fcae43cac0f174f0fb353
bala-amypo <bala@amypo.in>
2025-12-18 17:49:55 +0000

    User commit

--- src/main/java/com/example/demo/controller/VehicleController.java
+++ src/main/java/com/example/demo/controller/VehicleController.java
@@ -2,16 +2,16 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annonation.RestController;
 import org.springframework.beans.factory.annonation.Autowired;
 import com.example.demo.entity.Vehicle;
-import com.example.demo.service.ServiceEntryService;
+import com.example.demo.service.VehicleService;
 import org.springframework.web.bind.annonation.PostMapping;
 import org.springframework.web.bind.annonation.RequestBody;
 
 @RestController
-public class ServiceEntry{
+public class Vehicle{
     @Autowired
-    ServiceEntryService ses;
-    @PostMapping("/ServiceEntry")
-    public ServiceEntry addServiceEntry(@RequestBody ServiceEntry se){
-        return ses.createServiceEntry(se);
+    VehicleService vs;
+    @PostMapping("/Vehicle")
+    public Vehicle addVehicle(@RequestBody Vehicle v){
+        return vs.createVehicle(v);
     }
 }
