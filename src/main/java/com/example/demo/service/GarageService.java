16ba22bdeb1d279e7d93afa7f9c1fd10463ea312
bala-amypo <bala@amypo.in>
2025-12-19 06:16:48 +0000

    User commit

--- src/main/java/com/example/demo/service/GarageService.java
+++ src/main/java/com/example/demo/service/GarageService.java
@@ -1,3 +1,10 @@
 package com.example.demo.service;
 import jakarta.persistence.Service;
-import com.e
+import com.example.demo.model.Garage;
+@Service
+public interface Garage{
+    public Garage register(Garage garage);
+
+
+    public Garage findByEmail(String email);
+}
