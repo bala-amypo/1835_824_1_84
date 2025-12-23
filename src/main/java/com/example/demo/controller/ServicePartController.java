cd166f1e433245694919bfb30fbac0bdbabf360d
bala-amypo <bala@amypo.in>
2025-12-23 16:13:59 +0000

    User commit

--- src/main/java/com/example/demo/controller/ServicePartController.java
+++ src/main/java/com/example/demo/controller/ServicePartController.java
@@ -1,17 +1,37 @@
 package com.example.demo.controller;
+
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import com.example.demo.model.ServicePart;
 import com.example.demo.service.ServicePartService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 @RestController
-public class ServicePartController{
+@RequestMapping("/api/service-parts")
+public class ServicePartController {
+
     @Autowired
-    private ServicePartService sps;
-    @PostMapping("/ServicePart")
-    public ServicePart addServicePart(@RequestBody ServicePart sp){
-        return sps.createServicePart(sp);
+    private ServicePartService servicePartService;
+
+    @PostMapping("/")
+    public ServicePart addServicePart(@RequestBody ServicePart servicePart) {
+        return servicePartService.createServicePart(servicePart);
+    }
+
+    @GetMapping("/{id}")
+    public ServicePart getPartById(@PathVariable Long id) {
+        return servicePartService.getPartById(id);
+    }
+
+    @GetMapping("/entry/{entryId}")
+    public List<ServicePart> getPartsForEntry(@PathVariable Long entryId) {
+        return servicePartService.getPartsForEntry(entryId);
     }
 }
