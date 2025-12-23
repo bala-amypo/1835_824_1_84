56b450eaa81e55502c07f293a57c6039deefa606
bala-amypo <bala@amypo.in>
2025-12-23 08:09:22 +0000

    User commit

--- src/main/java/com/example/demo/repository/VehicleRepository.java
+++ src/main/java/com/example/demo/repository/VehicleRepository.java
@@ -1,11 +1,13 @@
 package com.example.demo.repository;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import com.example.demo.model.Vehicle;
 import java.util.List;
-public interface VehicleRepository extends JpaRepository<Vehicle,Long>{
-@Query("FROM Vehicle v where v.vin= :name")
-List<Vehicle> findByOwner(@Param("name") String name);
 
+public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
+
+    @Query("SELECT v FROM Vehicle v WHERE v.vin = :vin")
+    List<Vehicle> findByVin(@Param("vin") String vin);
 }
