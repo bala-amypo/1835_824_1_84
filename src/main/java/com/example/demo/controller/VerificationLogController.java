b0aed9373bfa528020ee1364c2fa70a6c4439524
bala-amypo <bala@amypo.in>
2025-12-24 17:54:56 +0000

    User commit

--- src/main/java/com/example/demo/controller/VerificationLogController.java
+++ src/main/java/com/example/demo/controller/VerificationLogController.java
@@ -1,16 +1,10 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.beans.factory.annotation.Autowired;
-
 import com.example.demo.model.VerificationLog;
 import com.example.demo.service.VerificationLogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PathVariable;
 import java.util.List;
 
 @RestController
